816dce90fe0bda518b76dbd9da991f9eda1980e5
Dario Ceballos <dario_tec_aut@hotmail.com.ar>
2019-06-10 16:17:46 -0300

    Distribucion a partir de Clases y Objetos
    Poseera una clase para el menu y de ahi las distintas clases con los didas siguientes.

---
+++ Instituto/Instituto/Dia1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Instituto
+{
+    class Dia1
+    {
+        static void Main(string[] args)
+        {
+            Dia3calculadoraDeEdad dia3 = new Dia3calculadoraDeEdad();
+            dia3.edad();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Instituto/Instituto/Dia3calculadoraDeEdad.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Instituto
+{
+    class Dia3calculadoraDeEdad
+    {
+        public void edad()
+        {
+            int fecha1, fecha2, edad;
+
+            Console.WriteLine("-------------------");
+            Console.WriteLine("CALCULADORA DE EDAD");
+            Console.WriteLine("-------------------");
+
+
+            Console.WriteLine("Ingrese el año actual");
+            fecha1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Ingrese el año de nacimiento");
+            fecha2 = Convert.ToInt32(Console.ReadLine());
+
+            edad = fecha1 - fecha2;
+
+            Console.WriteLine("Su edad es: " + edad);
+
+            if (edad >= 18)
+            {
+                Console.WriteLine("Usted es mayor de edad");
+            }
+            else
+            {
+                Console.WriteLine("Usted es menor de edad");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
