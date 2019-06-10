5dd56f4cdae2ca2b97a953dd1215793e85957863
Dario Ceballos <dario_tec_aut@hotmail.com.ar>
2019-06-10 17:06:42 -0300

    Seleccion de caso con SWITCH

--- Instituto/Instituto/Dia1.cs
+++ Instituto/Instituto/Dia1.cs
@@ -11,16 +11,34 @@ namespace Instituto
     {
         static void Main(string[] args)
         {
-            //Dia3calculadoraDeEdad dia3 = new Dia3calculadoraDeEdad();
-            //dia3.edad();
+          
+            int dia;
+            Inicio:
+            Console.WriteLine("Seleccione un dia tecleando un numero del 2 al 4 \n \n mas adelante se agregara un menu");
+            dia = Convert.ToInt32(Console.ReadLine());
 
-            //Dia2CalculadoraMat dia2 = new Dia2CalculadoraMat();
-            //dia2.cuenta();
+            switch (dia)
+            {
+                case 2:
+                    Dia2CalculadoraMat dia2 = new Dia2CalculadoraMat();
+                    dia2.cuenta();
+                    break;
+                case 3:
+                    Dia3calculadoraDeEdad dia3 = new Dia3calculadoraDeEdad();
+                    dia3.edad();
+                    break;
+                case 4:
+                    Dia4TablaMat dia4 = new Dia4TablaMat();
+                    dia4.tablass();
+                    break;
+                default:
+                    Console.WriteLine("ERROR ERROR ERROR ERROR ERROR ERROR \n  SELECCIONE UNA DE LAS OPCIONES \n\n\n");
+                    
+                    break;
 
-            Dia4TablaMat dia4 = new Dia4TablaMat();
-            dia4.tablass();
-
-            Console.ReadKey();
+            }
+            goto Inicio;
+                    Console.ReadKey();
         }
     }
 }
