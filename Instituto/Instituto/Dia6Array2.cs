e97b86b82325f6a182592ced14ec30ab3c696d58
Administrador <Administrador@WIN-IAN63RRJGEE>
2019-06-14 21:57:36 -0300

    ejercicio array.
    escribir 0101010
    con un bucle

--- Instituto/Instituto/Dia6Array2.cs
+++ Instituto/Instituto/Dia6Array2.cs
@@ -10,8 +10,24 @@ namespace Instituto
     {
         public void array2()
         {
+            int[] prueba = new int[7];
+         
 
-            Console.WriteLine("hola mundo");
+            for (int i = 0; i <= 6; i++)
+            {
+                if (i % 2 == 0)
+                    prueba[i] = 0;
+                else
+                    prueba[i] = 1;
+            }
+
+            for (int i = 0; i <= 6; i++)
+            {
+                Console.Write(prueba[i]);
+            }
+           
+            Console.ReadKey();
+
             Console.ReadLine();
 
         }
