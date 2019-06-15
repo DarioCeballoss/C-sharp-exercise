3a5dfb290e6528f13a47c638bdac3472574a5ab9
Administrador <Administrador@WIN-IAN63RRJGEE>
2019-06-14 21:13:28 -0300

    Dia 6 agregado mas hola mundo del dia 6

--- Instituto/Instituto/Dia1.cs
+++ Instituto/Instituto/Dia1.cs
@@ -12,41 +12,42 @@ namespace Instituto
         static void Main(string[] args)
         {
 
+            Dia6Array2 dia6 = new Dia6Array2();
+            dia6.array2();
 
 
+            //    int dia;
+            //Inicio:
+            //    Console.Clear();
+            //    Console.WriteLine("Seleccione un dia tecleando un numero del 2 al 5 \n \n mas adelante se agregara un menu");
+            //    dia = Convert.ToInt32(Console.ReadLine());
 
-            int dia;
-        Inicio:
-            Console.Clear();
-            Console.WriteLine("Seleccione un dia tecleando un numero del 2 al 5 \n \n mas adelante se agregara un menu");
-            dia = Convert.ToInt32(Console.ReadLine());
+            //    switch (dia)
+            //    {
+            //        case 2:
+            //            Dia2CalculadoraMat dia2 = new Dia2CalculadoraMat();
+            //            dia2.cuenta();
+            //            break;
+            //        case 3:
+            //            Dia3calculadoraDeEdad dia3 = new Dia3calculadoraDeEdad();
+            //            dia3.edad();
+            //            break;
+            //        case 4:
+            //            Dia4TablaMat dia4 = new Dia4TablaMat();
+            //            dia4.tablass();
+            //            break;
+            //        case 5:
+            //            Dia5Array dia5 = new Dia5Array();
+            //            dia5.Array();
+            //            break;
+            //        default:
+            //            Console.WriteLine("ERROR ERROR ERROR ERROR ERROR ERROR \n  SELECCIONE UNA DE LAS OPCIONES \n\n\n");
 
-            switch (dia)
-            {
-                case 2:
-                    Dia2CalculadoraMat dia2 = new Dia2CalculadoraMat();
-                    dia2.cuenta();
-                    break;
-                case 3:
-                    Dia3calculadoraDeEdad dia3 = new Dia3calculadoraDeEdad();
-                    dia3.edad();
-                    break;
-                case 4:
-                    Dia4TablaMat dia4 = new Dia4TablaMat();
-                    dia4.tablass();
-                    break;
-                case 5:
-                    Dia5Array dia5 = new Dia5Array();
-                    dia5.Array();
-                    break;
-                default:
-                    Console.WriteLine("ERROR ERROR ERROR ERROR ERROR ERROR \n  SELECCIONE UNA DE LAS OPCIONES \n\n\n");
+            //            break;
 
-                    break;
+            //    }
+            //    goto Inicio;
 
-            }
-            goto Inicio;
-            
         }
     }
 }
---
+++ Instituto/Instituto/Dia6Array2.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Instituto
+{
+    class Dia6Array2
+    {
+        public void array2()
+        {
+
+            Console.WriteLine("hola mundo");
+            Console.ReadLine();
+
+        }
+    }
+}
